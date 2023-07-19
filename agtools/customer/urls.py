7c8ee490897d293fdfbbe172c76d53d13f6a84ff
B21Munhoz <munhoz-alvaro@hotmail.com>
2023-07-19 11:54:25 -0300

    :art: Improving templates appearence

--- agtools/customer/urls.py
+++ agtools/customer/urls.py
@@ -5,6 +5,6 @@ from customer.views import CustomerTemplateView, CustomerFormTemplateView
 app_name = 'customers'
 
 urlpatterns = [
-    path('', CustomerTemplateView.as_view(), name='customers'),
+    path('', CustomerTemplateView.as_view(), name='list'),
     path('create/', CustomerFormTemplateView.as_view(), name='create'),
 ]
